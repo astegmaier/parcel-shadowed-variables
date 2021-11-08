e63856aea2c703b4d1c482af46df603986580462
Andrew Stegmaier <andrew.stegmaier@gmail.com>
2021-11-08 15:06:21 -0500

    Cleanup.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ var myNum = 32;
 
 function testShadowing() {
     let myNum = 999;
-    console.log("Shadowed myNum is " + myNum); // 32
-    console.log("Global myNum is " + window.myNum); // Should be 999, parcel outputs "undefined"
+    console.log("Shadowed myNum is " + myNum); // 999
+    console.log("Global myNum is " + window.myNum); // Should be 32, parcel outputs "undefined"
 }
 
 testShadowing();
